8123ef9dd80d97a95c2ca55faec5094c49d003de
thatoneham <106480611+thatoneham@users.noreply.github.com>
2025-01-15 16:31:12 +0000

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,9 +1,11 @@
 from websocket_server import WebsocketServer
 import time
 import json
+import uuid
 def new_client(client, server):
-	print("New client connected and was given id %d" % client['id'])
-	server.send_message_to_all("Hey all, a new client has joined us")
+    print(uuid.uuid4())
+    print("New client connected and was given id %d" % client['id'])
+    server.send_message_to_all("Hey all, a new client has joined us")
 
 
 def client_left(client, server):
